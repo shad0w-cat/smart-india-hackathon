3849b0c8d1f8e2ff4ce3642ce95d0a5b85d5efe9
Shubham-Yadav <shubhamyadav7063@gmail.com>
2022-07-28 11:59:03 +0530

    added home page buttons

---
+++ src/components/HomePage/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import "./button.css"
+function Button (){ 
+  return (
+    <div className='container'>
+        <button>Admin</button> 
+        <button>User</button> 
+    </div>
+  ); 
+};
+
+export default Button;
---
+++ src/components/HomePage/Home.js
@@ -0,0 +1,13 @@
+
+import Button from './Button'
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <Button/>
+        </div>
+
+    )
+}
+
+export default Home
